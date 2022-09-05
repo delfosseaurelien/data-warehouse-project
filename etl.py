c17b13456bfd1ff693b3ed53ba65823a738f9c86
a.delfosse <a.delfosse@instadeep.com>
2022-09-05 12:36:53 +0200

    first commit

---
+++ etl.py
@@ -0,0 +1,39 @@
+import configparser
+
+import psycopg2
+
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur: psycopg2.cursor, conn: psycopg2.connection) -> None:
+    """Execute queries"""
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur: psycopg2.cursor, conn: psycopg2.connection):
+    """Execute insert table queries"""
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read("dwh.cfg")
+
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config["CLUSTER"].values()
+        )
+    )
+    cur = conn.cursor()
+
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
